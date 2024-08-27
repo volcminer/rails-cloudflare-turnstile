092f9260d44a5077411a015035c77b05ae02fcb9
charlsmagic <44356145+charlsmagic@users.noreply.github.com>
2024-08-27 13:53:00 +0800

    Update engine.rb
    ignore api only

--- lib/rails_cloudflare_turnstile/engine.rb
+++ lib/rails_cloudflare_turnstile/engine.rb
@@ -1,8 +1,10 @@
 module RailsCloudflareTurnstile
   class Engine < ::Rails::Engine
     initializer "rails_cloudflare_turnstile.precompile" do |app|
-      %w[javascripts images].each do |sub|
-        app.config.assets.paths << root.join("assets", sub).to_s
+      unless app.config.api_only
+        %w[javascripts images].each do |sub|
+          app.config.assets.paths << root.join("assets", sub).to_s
+        end
       end
     end
   end
